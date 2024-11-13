fe5d743d4d1838e44d7e74c72090f762d2ce49a4
Eugene Dechko <eugene1402d@gmail.com>
2024-11-13 11:05:55 +0300

    One more commit

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
 print("I’m planning to learn git conflicts resolution")
 print("Введите строку для изменения файла в ветке work")
 print("Изменение файла в рабочей ветке")
+
+
+print("Добавление изменения")
